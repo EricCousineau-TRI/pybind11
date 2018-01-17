bd7489c6f048ad81037de16f3321895b253027a5
Eric Cousineau <eric.cousineau@tri.global>
2018-01-17 01:01:43 -0500

    Hide details of using `wrapper<>`.

--- tests/test_ownership_transfer.cpp
+++ tests/test_ownership_transfer.cpp
@@ -75,9 +75,9 @@ class DefineBaseUniqueContainer {
 };
 
 template <int label>
-class DefinePyBase : public py::wrapper<DefineBase<label>> {
+class DefinePyBase : public DefineBase<label> {
  public:
-  using BaseT = py::wrapper<DefineBase<label>>;
+  using BaseT = DefineBase<label>;
   using BaseT::BaseT;
   int value() const override {
     PYBIND11_OVERLOAD(int, BaseT, value);
@@ -135,6 +135,7 @@ template <typename... Args>
 using class_unique_ = py::class_<Args...>;
 
 TEST_SUBMODULE(ownership_transfer, m) {
+  // No alias - will not have lifetime extended.
   class_shared_<BaseBad>(m, "BaseBad")
       .def(py::init<int>())
       .def("value", &BaseBad::value);
@@ -144,6 +145,7 @@ TEST_SUBMODULE(ownership_transfer, m) {
       .def("release", &BaseBadContainer::release);
   class_shared_<ChildBadStats>(m, "ChildBadStats");
 
+  // Has alias - will have lifetime extended.
   class_shared_<Base, PyBase>(m, "Base")
       .def(py::init<int>())
       .def("value", &Base::value);
