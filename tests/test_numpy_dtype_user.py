7a84de1672f3c4e9dfa43e1c8b154400071480eb
Eric Cousineau <eric.cousineau@tri.global>
2018-03-14 17:49:20 -0400

    developing test

--- tests/test_numpy_dtype_user.py
+++ tests/test_numpy_dtype_user.py
@@ -1,14 +1,18 @@
 import gc
 import sys
+import numpy as np
 
 import pytest
 from pybind11_tests import ConstructorStats
 from pybind11_tests import numpy_dtype_user as m
 
-stats_c = ConstructorStats.get(m.Custom)
+stats = ConstructorStats.get(m.Custom)
 # stats_str = ConstructorStats.get(m.CustomStr)
 
-def test_dtype_instance():
+def check_dtype(a, b):
+    assert np.dtype(a) == np.dtype(b)
+
+def test_scalar_ctor():
     """ Tests a single scalar instance. """
     c = m.Custom()
     c1 = m.Custom(10)
@@ -20,9 +24,45 @@ def test_dtype_instance():
     del c2
     pytest.gc_collect()
     print('wooh')
+    assert stats.alive() == 0
+ 
+def test_scalar_meta():
+    assert issubclass(m.Custom, np.generic)
+    assert isinstance(np.dtype(m.Custom), np.dtype)
+
+def test_scalar_op():
+    
+
+def test_array_creation():
+    # Zeros.
+    x = np.zeros((2, 2), dtype=m.Custom)
+    assert x.shape == (2, 2)
+    assert x.dtype == m.Custom
+    # Generic creation.
+    x = np.array([m.Custom(1)])
+    assert x.dtype == m.Custom
+    # - Limitation on downcasting.
+    x = np.array([m.Custom(1), 1])
+    assert x.dtype == object
+
+def test_array_cast():
+    x = np.array([m.Custom(1)])
+    def check(dtype):
+        dx = x.astype(dtype)
+        assert dx.dtype == dtype, dtype
+        assert dx.astype(m.Custom).dtype == m.Custom
+    check(m.Custom)
+    check(float)
+    check(object)
+
+def test_array_ufunc():
+
 
 # sys.stdout = sys.stderr
 # sys.argv = [__file__, "-s"]
 # pytest.main(args=sys.argv[1:])
 pytest.gc_collect = gc.collect
-test_dtype_instance()
+test_scalar_ctor()
+test_scalar_meta()
+test_array_creation()
+test_array_cast()
