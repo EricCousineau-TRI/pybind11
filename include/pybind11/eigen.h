3e7761fc54fd8832affbc8160a41b25f8ac7a7a9
Eric Cousineau <eric.cousineau@tri.global>
2018-03-23 08:56:48 -0400

    eigen: Use general check for dtype=object

--- include/pybind11/eigen.h
+++ include/pybind11/eigen.h
@@ -223,7 +223,7 @@ template <typename props> handle eigen_array_cast(typename props::Type const &sr
     constexpr ssize_t elem_size = sizeof(typename props::Scalar);
     array a;
     using Scalar = typename props::Type::Scalar;
-    bool is_pyoject = static_cast<pybind11::detail::npy_api::constants>(npy_format_descriptor<Scalar>::value) == npy_api::NPY_OBJECT_;
+    bool is_pyoject = static_cast<pybind11::detail::npy_api::constants>(npy_format_descriptor<Scalar>::dtype().num()) == npy_api::NPY_OBJECT_;
 
     if (!is_pyoject) {
         if (props::vector)
