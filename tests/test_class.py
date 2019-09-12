9bdf0d37eb8cf0d9ccc25d9372874285e454a49a
Eric Cousineau <eric.cousineau@tri.global>
2019-09-12 14:48:29 -0400

    add unnitest.... Fails?

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,4 +1,5 @@
 import pytest
+import weakref
 
 from pybind11_tests import class_ as m
 from pybind11_tests import UserType, ConstructorStats
@@ -289,3 +290,47 @@ def test_aligned():
     if hasattr(m, "Aligned"):
         p = m.Aligned().ptr()
         assert p % 1024 == 0
+
+
+def test_drake_11424():
+
+    # A scope which can be used to define items.
+    class Scope(object): pass
+
+    scope = Scope()
+    m.def_virtual_c1(scope)
+
+    class C1(scope.VirtualC1):
+        def __init__(self): scope.VirtualC1.__init__(self)
+        def get_name(self): return "C1"
+
+    # # This should be the pointer for the classes.
+    # ids_1 = (id(scope.VirtualC1), id(C1))
+
+    # assert scope.VirtualC1().get_name() == "VirtualC1"
+    # assert C1().get_name() == "C1"
+
+    # # Now delete everything (and ensure it's deleted).
+    scope_w = weakref.ref(scope)
+    C1_w = weakref.ref(C1)
+    del scope
+    del C1  # Removing this fixes: `TypeError: __init__(self, ...) called with invalid `self` argument"
+    pytest.gc_collect()
+    assert scope_w() is None
+    assert C1_w() is None
+
+    # Now repeat.
+
+    scope = Scope()
+    m.def_virtual_c2(scope)
+
+    class C2(scope.VirtualC2):
+        def __init__(self): scope.VirtualC2.__init__(self)
+        def get_name(self): return "C2"
+
+    ids_2 = (id(scope.VirtualC2), id(C2))
+    print(ids_1)
+    print(ids_2)
+
+    assert scope.VirtualC2().get_name() == "VirtualC2"
+    assert C2().get_name() == "C2"
