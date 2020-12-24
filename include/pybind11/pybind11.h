fc0130d466e5175f1815f329e0bb8a55b94a7ea4
Eric Cousineau <eric.cousineau@tri.global>
2020-12-24 13:46:07 -0600

    still can't define globals...

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -1572,48 +1572,25 @@ inline str enum_name(handle arg) {
 struct enum_meta_info {
     pybind11::object enum_meta_cls;
     pybind11::object enum_base_cls;
+    pybind11::dict globals;
     pybind11::dict locals;
 
     enum_meta_info() {
+        pybind11::dict actual_globals = pybind11::globals();
+        globals = pybind11::dict();
+        for (auto pair : actual_globals) {
+            globals[pair.first] = pair.second;
+        }
         locals["pybind11_meta_cls"] = reinterpret_borrow<object>(
             reinterpret_cast<PyObject*>(get_internals().default_metaclass));
         locals["pybind11_base_cls"] = reinterpret_borrow<object>(
             get_internals().instance_base);
-        PyObject *result = PyRun_String(R"""(
-try:
-    import enum
-except ModuleNotFoundError:
-    enum = None
-
-pybind11_enum_base_cls = pybind11_base_cls
-pybind11_enum_meta_cls = pybind11_meta_cls
-
-if enum is not None:
-
-    def _instancecheck(cls, instance):
-        subclass = type(instance)
-        return issubclass(subclass, cls)
-
-    def _subclasscheck(cls, subclass):
-        if type.__subclasscheck__(enum.Enum, cls) and type.__subclasscheck__(cls, subclass):
-            return True
-        elif type.__subclasscheck__(pybind11_base_cls, cls):
-            is_pybind11_enum = getattr(subclass, '_is_pybind11_enum')
-            return is_pybind11_enum == True
-        else:
-            return False
-
-    enum.EnumMeta.__instancecheck__ = _instancecheck
-    enum.EnumMeta.__subclasscheck__ = _subclasscheck
-
-
-class pybind11_enum_meta_cls(pybind11_meta_cls):
-    def __iter__(cls):
-        return iter(cls.__members__.values())
-
-    def __len__(cls):
-        return len(cls.__members__)
-)""", Py_file_input, globals().ptr(), locals.ptr());
+        const char* filename = "/home/eacousineau/proj/tri/repo/externals/pybind11/include/pybind11/pybind11_internals.py";
+        FILE* fd = fopen(filename, "r");
+        int closeit = 1;
+        int start = Py_file_input;
+        PyObject *result = PyRun_FileEx(
+            fd, filename, start, globals.ptr(), locals.ptr(), closeit);
         if (result == nullptr) {
             throw error_already_set();
         }
---
+++ include/pybind11/pybind11_internals.py
@@ -0,0 +1,33 @@
+try:
+    import enum
+except ModuleNotFoundError:
+    enum = None
+
+if enum is not None:
+
+    def _instancecheck(cls, instance):
+        subclass = type(instance)
+        return issubclass(subclass, cls)
+
+    def _subclasscheck(cls, subclass):
+        if type.__subclasscheck__(enum.Enum, cls) and type.__subclasscheck__(cls, subclass):
+            return True
+        elif type.__subclasscheck__(pybind11_base_cls, cls):
+            is_pybind11_enum = getattr(subclass, '_is_pybind11_enum')
+            return is_pybind11_enum == True
+        else:
+            return False
+
+    enum.EnumMeta.__instancecheck__ = _instancecheck
+    enum.EnumMeta.__subclasscheck__ = _subclasscheck
+
+
+class pybind11_enum_meta_cls(pybind11_meta_cls):
+    def __iter__(cls):
+        return iter(cls.__members__.values())
+
+    def __len__(cls):
+        return len(cls.__members__)
+
+
+pybind11_enum_base_cls = None
